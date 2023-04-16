2471457832f6d39b062cdefd4da0a8fed98510cf
Dooy <dooy520@qq.com>
2023-04-16 17:06:16 +0800

    seo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,8 +34,7 @@
                 <div class="footer-inner">
                     <!-- Add your copyright text here -->
                     <div class="footer-text">
-                        &copy; 2023 AI网站导航 <a href="/about" target="_blank">欢迎申请入驻AI导航</span>
-                         
+                        &copy; 2023 <b>AI网站导航</b>  <a href="/about" target="_blank" style="color: #409EFF;">欢迎申请入驻AI导航</a>
                     </div>
                     <!-- Go to Top Link, just add rel="go-top" to any link to add this functionality -->
                     <div class="go-up">
